44dd523dfdd8e91319684e9f338321f0d909556e
可可同学 <AndroidLeo@outlook.com>
2022-05-06 11:31:56 +0800

    card-text-tag

--- src/components/foo-card-text/index.tsx
+++ src/components/foo-card-text/index.tsx
@@ -1,9 +1,9 @@
 import './card-text.less'
 
 import { CardText } from './card-text'
-import { CardTextTag } from './card.text.tag'
+import { CardTextTag } from './card-text-tag'
 
-export type { CardTextTagProps } from './card.text.tag'
+export type { CardTextTagProps } from './card-text-tag'
 
 export type { CardTextProps } from './card-text'
 
